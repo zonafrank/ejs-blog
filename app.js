65b05c2f0e51108c62dd5e0e33fb35c58e4cc504
Eche Oji <echyth@yahoo.com>
2022-08-21 12:35:28 +0100

    fix port definition error

--- app.js
+++ app.js
@@ -77,8 +77,8 @@ app.post("/compose", async (request, response) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(PORT, function () {
+  console.log("Server started on port ${PORT}");
 });
 
 process.on("SIGINT", function () {
